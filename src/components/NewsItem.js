1db1c9703f385fc72238a13812c9adaa3f07aba4
mitushi <mitushi2agrawal@gmail.com>
2021-10-15 15:39:00 +0530

    Modified badge in newsitem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -14,11 +14,11 @@ export class NewsItem extends Component {
                     }
                     }>
 
-                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                        
                     </div>
                     <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}  </h5>
+                        <h5 className="card-title">{title} <span className="badge rounded-pill bg-success"> {source} </span> </h5>
                         <p className="card-text">{description}</p>
                         <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
